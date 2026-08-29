df2b268e176e9e3a36356ba01a20229949f6b2d6
ayushmandwivedi <ayushman14dwivedi@gmail.com>
2026-08-30 01:09:54 +0530

    minor fix related to removing of unused parameter

--- src/store/BoardProvider.js
+++ src/store/BoardProvider.js
@@ -209,7 +209,7 @@ const BoardProvider = ({ children }) => {
     });
   };
 
-  const textAreaBlurHandler = (text, toolboxState) => {
+  const textAreaBlurHandler = (text) => {
     dispatchBoardAction({
       type: BOARD_ACTIONS.CHANGE_TEXT,
       payload: {
